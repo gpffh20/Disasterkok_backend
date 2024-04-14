a51170d1c5ea2dd8d7064f63080f2620d4a6fad5
gpffh20 <eunbin584836@gmail.com>
2024-04-14 22:13:41 +0900

    fix: google login 동작하도록 수정

--- webapp/user/urls.py
+++ webapp/user/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path("login/", LoginAPIView.as_view()), # post - 로그인
     path("logout/", LogoutAPIView.as_view()), # post - 로그아웃
 
-    path('google/login/', googleLogin.as_view(), name='google_login'), # get - 구글 로그인
-    path('google/callback/', googleCallback.as_view(), name='google_callback'), # get - 구글 콜백
+    path('google/login/', googleLogin), # get - 구글 로그인
+    path('google/callback/', googleCallback), # get - 구글 콜백
 
     path("kakao/login/", kakaoLogin), # get - 카카오 로그인
     path("kakao/callback/", kakaoCallback), # get - 카카오 콜백
--- webapp/user/views/googleLoginUserView.py
+++ webapp/user/views/googleLoginUserView.py
@@ -1,94 +1,80 @@
-import os
-from json import JSONDecodeError
-
-import requests
-from django.core.exceptions import ValidationError
+import os, requests
 from django.shortcuts import redirect
 from rest_framework import status
-from rest_framework.decorators import permission_classes
-from rest_framework.exceptions import ValidationError
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from user.models import *
+from user.models import User
 
-# 구글 소셜로그인 변수 설정
-state = os.environ.get("STATE")
-BASE_URL = 'http://localhost:8888/'
-GOOGLE_CALLBACK_URI = BASE_URL + 'users/google/callback/'
+import logging
 
-
-# 구글 로그인
+@api_view(["GET"])
 @permission_classes([AllowAny])
-class googleLogin(APIView):
-    def get(self, request, *args, **kwargs):
-        scope = "https://www.googleapis.com/auth/userinfo.profile%20https://www.googleapis.com/auth/userinfo.email"
-        client_id = os.environ.get("GOOGLE_CLIENT_ID")
-        return redirect(
-            f"https://accounts.google.com/o/oauth2/v2/auth?client_id={client_id}&response_type=code&redirect_uri={GOOGLE_CALLBACK_URI}&scope={scope}")
+def googleLogin(request):
+    CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
+    REDIRECT_URL = os.environ.get("GOOGLE_REDIRECT_URL")
+    STATE = "RANDOM_STATE"
+    SCOPE = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
+    url = (f"https://accounts.google.com/o/oauth2/v2/auth?response_type=code"
+           f"&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URL}&scope={SCOPE}&state={STATE}")
+    return redirect(url)
 
-
-# 구글 callback
+@api_view(["GET"])
 @permission_classes([AllowAny])
-class googleCallback(APIView):
-    def get(self, request, *args, **kwargs):
-        client_id = os.environ.get("GOOGLE_CLIENT_ID")
-        client_secret = os.environ.get("GOOGLE_SECRET")
-        code = request.GET.get('code')
-        token_req = requests.post(
-            f"https://oauth2.googleapis.com/token",
-            headers={"Content-Type": "application/x-www-form-urlencoded"},
-            data={
-                "client_id": client_id,
-                "client_secret": client_secret,
-                "code": code,
-                "grant_type": "authorization_code",
-                "redirect_uri": GOOGLE_CALLBACK_URI,
-                "state": state
-            }
-        )
-        access_token = token_req.json().get('access_token')
-        error = token_req.json().get("error")
-        if error is not None:
-            raise JSONDecodeError(error)
-        user_req = requests.get(f"https://www.googleapis.com/oauth2/v2/tokeninfo?access_token={access_token}")
-        if not user_req.ok:
-            raise ValidationError('Failed to obtain user info from Google.')
+def googleCallback(request):
+    client_id = os.environ.get("GOOGLE_CLIENT_ID")
+    client_secret = os.environ.get("GOOGLE_SECRET")
+    code = request.GET.get("code")
+    state = request.GET.get("state")
+    redirect_uri = os.environ.get("GOOGLE_REDIRECT_URL")
 
-        user_req_json = user_req.json()
-        email = user_req_json.get('email')
-        user_id = user_req_json.get('user_id')
-        try:
-            user = User.objects.get(googleID=email)
+    if code is None:
+        raise Exception("Authorization code is missing.")
+
+    url = "https://oauth2.googleapis.com/token"
+    data = {
+        'code': code,
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'redirect_uri': redirect_uri,
+        'grant_type': 'authorization_code',
+    }
+
+    token_response = requests.post(url, data=data)
+    token_json = token_response.json()
+    access_token = token_json.get("access_token")
+
+    if 'error' in token_json:
+        error_description = token_json.get('error_description', 'No description provided.')
+        logging.error(f"Failed to retrieve access token: {error_description}")
+        return Response({"message": f"Failed to retrieve access token: {error_description}"},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    if access_token:
+        userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+        headers = {"Authorization": f"Bearer {access_token}"}
+        profile_response = requests.get(userinfo_url, headers=headers)
+        profile_json = profile_response.json()
+
+        googleId = profile_json.get("id")
+        username = profile_json.get("email")
+        email = profile_json.get("email")
+
+        if googleId:
+            user, created = User.objects.get_or_create(googleId=googleId, defaults={'username': username, 'email': email})
             refresh = RefreshToken.for_user(user)
             data = {
                 "user": {
                     "id": user.id,
-                    "googleID": user.googleID,
+                    "googleId": user.googleId,
                     "username": user.username,
+                    "email": user.email,
                 },
-                "token": {
-                    "access": f"{str(refresh.access_token)}",
-                    "refresh": f"{str(refresh)}"
-                }
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
             }
             return Response(data, status=status.HTTP_200_OK)
 
-        except User.DoesNotExist:
-            User(googleID=email, username=user_id).save()
-            user = User.objects.get(googleID=email)
-            refresh = RefreshToken.for_user(user)
-            data = {
-                "user": {
-                    "id": user.id,
-                    "googleId": user.googleID,
-                    "username": user.username,
-                },
-                "token": {
-                    "access": f"{str(refresh.access_token)}",
-                    "refresh": f"{str(refresh)}"
-                }
-            }
-            return Response(data, status=status.HTTP_200_OK)
+    return Response({"message": "Failed to retrieve information from Google."}, status=status.HTTP_400_BAD_REQUEST)
